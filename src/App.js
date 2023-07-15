74777540b06714b5c9fa68d7103f96dee44cda3a
Arjun Talwar <arjuntalwar1110@gmail.com>
2023-07-16 01:01:07 +0530

    new change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element = {<Home/>} />
+        <Route exact path="/about" element = {<About/>} />
+        </Routes>
+    </Router>
   )
 }
